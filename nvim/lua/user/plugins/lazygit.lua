833aef2e1da146ed2df702ce36d8ade098789439
Aniket Gargya <gargya.aniket@gmail.com>
2024-09-20 19:06:55 -0500

    add lazygit

---
+++ nvim/lua/user/plugins/lazygit.lua
@@ -0,0 +1,13 @@
+return {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+        "LazyGit",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+}
+
