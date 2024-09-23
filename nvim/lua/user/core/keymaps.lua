fd340db6ace1ccbe24c8c111ff03071729eb2e23
Aniket Gargya <gargya.aniket@gmail.com>
2024-09-22 21:23:34 -0500

    add command for lazy nvim

--- nvim/lua/user/core/keymaps.lua
+++ nvim/lua/user/core/keymaps.lua
@@ -9,3 +9,5 @@ keymap.set("n", "<leader>sh", "<C-w>v", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
+keymap.set("n", "<leader>ln", "<cmd>Lazy<cr>", { desc = "Lazy Nvim" })
+
