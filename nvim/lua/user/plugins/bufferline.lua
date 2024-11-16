1635ca6267920ec83f41bf510a59f18547cd622e
Aniket Gargya <gargya.aniket@gmail.com>
2024-11-16 02:10:52 -0600

    clean up bufferline config, add vim-bbye

--- nvim/lua/user/plugins/bufferline.lua
+++ nvim/lua/user/plugins/bufferline.lua
@@ -1,28 +1,29 @@
 return {
     "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-        local bufferline = require("bufferline")
-
-        bufferline.setup {
-            options = {
-                offsets = {
-                    { filetype = "NvimTree", text = "", padding = 1 },
-                },
-                separator_style = "thick",
-                buffer_background = "#000000",
-                buffer_foreground = "#ffffff",
-            }
-        }
-
-        local keymap = vim.keymap
-
-        keymap.set("n", "<leader>bo", "<cmd>enew<CR>", { desc = "Open new buffer" })
-        keymap.set("n", "<leader>bx", "<cmd>bd<CR>", { desc = "Close current buffer" })
-        keymap.set("n", "<leader>bp", "<cmd>BufferLineCyclePrev<CR>", { desc = "Go to previous buffer" })
-        keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<CR>", { desc = "Go to next buffer" })
-        keymap.set("n", "H", "<cmd>BufferLineCyclePrev<CR>", { desc = "Go to previous buffer" })
-        keymap.set("n", "L", "<cmd>BufferLineCycleNext<CR>", { desc = "Go to next buffer" })
-    end,
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "moll/vim-bbye",
+    },
+    opts = {
+        options = {
+            offsets = {
+                { filetype = "NvimTree", text = "", padding = 1 },
+            },
+            separator_style = "thick",
+            show_buffer_close_icons = false,
+            buffer_background = "#000000",
+            buffer_foreground = "#ffffff",
+            sort_by = "insert_after_current",
+        },
+    },
+    keys = {
+        { "<leader>bo", "<cmd>enew<CR>", desc = "Open new buffer" },
+        { "<leader>bx", "<cmd>bd<CR>", desc = "Close current buffer" },
+        { "<leader>bp", "<cmd>BufferLineCyclePrev<CR>", desc = "Go to previous buffer" },
+        { "<leader>bn", "<cmd>BufferLineCycleNext<CR>", desc = "Go to next buffer" },
+        { "H", "<cmd>BufferLineCyclePrev<CR>", desc = "Go to previous buffer" },
+        { "L", "<cmd>BufferLineCycleNext<CR>", desc = "Go to next buffer" },
+    },
 }
 
